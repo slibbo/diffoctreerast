b7e73d8965579d1bc050496d39a581bb66c7e0d4
slibbo <slibbo@hush.ai>
2025-02-11 21:46:00 -0800

    Hipify CUDA

--- setup.py
+++ setup.py
@@ -11,29 +11,29 @@ setup(
             name="diffoctreerast._C",
             sources=[
                 # Octree Voxel rasterization
-                "src/octree_voxel_rasterizer/cuda/data_structure.cu",
-                "src/octree_voxel_rasterizer/cuda/forward.cu",
-                "src/octree_voxel_rasterizer/cuda/backward.cu",
+                "src/octree_voxel_rasterizer/cuda/data_structure.cu.hip",
+                "src/octree_voxel_rasterizer/cuda/forward.cu.hip",
+                "src/octree_voxel_rasterizer/cuda/backward.cu.hip",
                 "src/octree_voxel_rasterizer/api.cpp",
                 # Octree Gaussian rasterization
-                "src/octree_gaussian_rasterizer/cuda/data_structure.cu",
-                "src/octree_gaussian_rasterizer/cuda/forward.cu",
-                "src/octree_gaussian_rasterizer/cuda/backward.cu",
+                "src/octree_gaussian_rasterizer/cuda/data_structure.cu.hip",
+                "src/octree_gaussian_rasterizer/cuda/forward.cu.hip",
+                "src/octree_gaussian_rasterizer/cuda/backward.cu.hip",
                 "src/octree_gaussian_rasterizer/api.cpp",
                 # Octree Trivec rasterization
-                "src/octree_trivec_rasterizer/cuda/data_structure.cu",
-                "src/octree_trivec_rasterizer/cuda/forward.cu",
-                "src/octree_trivec_rasterizer/cuda/backward.cu",
+                "src/octree_trivec_rasterizer/cuda/data_structure.cu.hip",
+                "src/octree_trivec_rasterizer/cuda/forward.cu.hip",
+                "src/octree_trivec_rasterizer/cuda/backward.cu.hip",
                 "src/octree_trivec_rasterizer/api.cpp",
                 # Octree Decoupled Polynomial rasterization
-                "src/octree_decoupoly_rasterizer/cuda/data_structure.cu",
-                "src/octree_decoupoly_rasterizer/cuda/forward.cu",
-                "src/octree_decoupoly_rasterizer/cuda/backward.cu",
+                "src/octree_decoupoly_rasterizer/cuda/data_structure.cu.hip",
+                "src/octree_decoupoly_rasterizer/cuda/forward.cu.hip",
+                "src/octree_decoupoly_rasterizer/cuda/backward.cu.hip",
                 "src/octree_decoupoly_rasterizer/api.cpp",
                 # Main
                 "src/ext.cpp"
             ],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/glm/")]})
+            extra_compile_args={"hipcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/glm/")]})
         ],
     cmdclass={
         'build_ext': BuildExtension
